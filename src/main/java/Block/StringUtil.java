0564e228277d5192147261c53f6bc68b468c77f6
Lelikbjmak <denissolovey990@gmail.com>
2023-02-03 22:47:30 +0300

    StringUtils for hash generating

---
+++ src/main/java/Block/StringUtil.java
@@ -0,0 +1,26 @@
+package Block;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+public class StringUtil {
+    //Applies Sha256 to a string and returns the result.
+    public static String applySha256(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //Applies sha256 to our input,
+            byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder(); // This will contain hash as hexidecimal
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
