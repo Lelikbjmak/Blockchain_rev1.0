f8d560729e387d5379e82d826bf86ff979624fe5
Lelikbjmak <denissolovey990@gmail.com>
2023-02-04 22:54:04 +0300

    description + eradicate error in mining func

--- src/main/java/Block/Block.java
+++ src/main/java/Block/Block.java
@@ -3,11 +3,11 @@ package Block;
 import java.util.Date;
 
 public class Block {
-    private String hash;
-    private final String previousHash;
-    private final String data;
-    private final long timeStamp;
-    private int nonce;
+    private String hash; // hash of current block
+    private final String previousHash;  // hash of previous block ti make chain
+    private final String data;   // data of current block (like BTC comprises amount to transfer etc.)
+    private final long timeStamp;  // timestamp of current block
+    private int nonce;  // typical counter (count of iterations were made to mine block)
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -17,11 +17,12 @@ public class Block {
     }
 
     public String calculateBlockHash() {
-        return StringUtil.applySha256(previousHash + timeStamp + data);
+        return StringUtil.applySha256(previousHash + timeStamp + nonce + data);
     }
 
     public void mineBlock(int difficulty) {
         String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        System.out.println(target);
         while(!hash.substring( 0, difficulty).equals(target)) {
             nonce ++;
             hash = calculateBlockHash();
